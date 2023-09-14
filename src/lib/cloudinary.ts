6d0ae518cad372dbfba243fb574fbb4bef658ef4
Oliver Saxon <oliverrsaxon@gmail.com>
2023-09-14 09:09:59 +0100

    initial commit

---
+++ src/lib/cloudinary.ts
@@ -0,0 +1,11 @@
+import cloudinary from "cloudinary";
+
+// @ts-ignore
+cloudinary.config({
+    cloud_name: import.meta.env.CLOUDINARY_CLOUD_NAME,
+    api_key: import.meta.env.CLOUDINARY_API_KEY,
+    api_secret: import.meta.env.CLOUDINARY_API_SECRET,
+    secure: true,
+});
+
+export default cloudinary;
